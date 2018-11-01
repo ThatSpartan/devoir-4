c22c20e9ab7b272a898f09f6d4d1fc1bc1df8db3
ThatSpartan <dodo4931@outlook.com>
2018-11-01 14:11:09 -0400

    init / completed q4d1

---
+++ q4d1.py
@@ -0,0 +1,27 @@
+def show_m(m):
+    out = ''
+    m = [[str(x) for x in l] for l in m]
+    mat_out = [' '.join(l) for l in m]
+    out = '\n  '.join(mat_out)
+
+    out = '[ ' + out + ' ]'
+
+    print(out)
+
+def add_one(m):
+    return [[n+1 for n in line] for line in m]
+
+def add_one_no_return(m):
+    for i in range(len(m)):
+        for j in range(len(m[i])):
+            m[i][j] += 1
+
+ma = [[1,2,3],[4,5,6]]
+ma_p1 = add_one(ma)
+show_m(ma)
+show_m(ma_p1)
+show_m(ma)
+print('---------')
+show_m(ma)
+add_one_no_return(ma)
+show_m(ma)
